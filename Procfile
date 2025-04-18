2ec5f6f6990ee598398e5d9d2365b6d4234c96f3
Misharik228 <krokodil22008@gmail.com>
2025-04-18 14:32:08 +0000

    Configure for Railway deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn todolist.wsgi:application --bind 0.0.0.0:$PORT
+web: gunicorn todolist.wsgi:application --workers 4 --worker-class=gthread --threads 2 --bind 0.0.0.0:$PORT
--- todolist/settings.py
+++ todolist/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
